30a55087c37b7646d622e26f1189bcc8870a00ea
dwitthold <witthold@menkar.uberspace.de>
2023-04-08 13:47:25 +0200

    Use main window as demo widget

--- src/main/java/net/unterwelt/MyApp.java
+++ src/main/java/net/unterwelt/MyApp.java
@@ -1,13 +1,24 @@
 package net.unterwelt;
 
 import io.qt.widgets.QApplication;
-import io.qt.widgets.QMessageBox;
+import io.qt.widgets.QCalendarWidget;
+import io.qt.widgets.QMainWindow;
+import io.qt.widgets.QWidget;
 
 public class MyApp {
 
     public static void main(String[] args) {
         QApplication.initialize(args);
-        QMessageBox.information(null, "QtJambi", "Moin World!");
+
+        QWidget calendar = new QCalendarWidget();
+
+        QMainWindow mainWindow = new QMainWindow();
+        mainWindow.setCentralWidget(calendar);
+        mainWindow.setFixedSize(300, 300);
+
+        mainWindow.show();
+        QApplication.exec();
+
         QApplication.shutdown();
     }
 }
